21b4c7c3c2042a219600cda8e49e1cee796d8775
Fuad Hassen <133159437+fuadhasen@users.noreply.github.com>
2024-06-28 23:26:40 +0530

    Update 1-install_a_package.pp

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,11 @@
 # manifest that install Flask package
 
-package {'flask':
+package { 'flask':
     ensure   => '2.1.0',
     provider => 'pip3'
+}
+
+package { 'werkzeug':
+  ensure   => '2.1.1',
+  provider => pip3,
 }
