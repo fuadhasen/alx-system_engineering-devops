2a0405f9d81b2fe75f8c6a44f2f87f4d54fe4408
fuadhasen <fuadhas6634@gmail.com>
2024-07-29 15:47:10 +0000

    add task 1: Export to csv

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+""" python script Export to CSV
+"""
+import csv
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    _id = sys.argv[1]
+
+    url = 'https://jsonplaceholder.typicode.com/users/{}'.format(_id)
+    response = requests.get(url)
+    if response.status_code == 200:
+        # dict contain user info like name ...
+        user = response.json()
+
+    url_2 = 'https://jsonplaceholder.typicode.com/users/{}/todos'.format(_id)
+    res = requests.get(url_2)
+    if res.status_code == 200:
+        # list of dict contain user info like tasks ...
+        tasks = res.json()
+
+    with open('{}.csv'.format(user.get('id')),
+            mode='w', encoding='utf-8') as _file:
+        writer = csv.writer(_file)
+        # format is in this way
+        # writer.writerow(["USER_ID","USERNAME","TASK_COMPLETED_STATUS","TASK_TITLE"])
+        for task in tasks:
+            writer.writerow([
+                str(task.get('userId')),
+                str(user.get('name')),
+                str(task.get('completed')),
+                str(task.get('title'))
+            ])
+
+
