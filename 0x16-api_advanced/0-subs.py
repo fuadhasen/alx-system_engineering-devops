49cbbc0771015a6c47cca20f3a4430cd75e7c147
fuadhasen <fuadhas6634@gmail.com>
2024-08-13 08:18:42 +0000

    add task 0

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+script fetch Reddit API and returns the number of subscribers
+"""
+
+import requests
+
+
+def number_of_subscribers(subreddit):
+    """function for api request"""
+    url = "https://api.reddit.com/r/{}/about".format(
+            subreddit)
+    headers = {'User-Agent': 'CustomClient/1.0'}
+
+    res = requests.get(url, headers=headers, allow_redirects=False)
+    if res.status_code != 200:
+        return 0
+
+    res = res.json()
+    data = res.get('data')
+    if data is None:
+        return 0
+
+    result = data.get('subscribers')
+    return result
